78891079a1ff5b8b2704f68e55318aa826736d43
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-08-06 18:16:46 -0300

    add: view snippet and create snippet boilerplates

--- main.go
+++ main.go
@@ -6,7 +6,26 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	// check if the current request URL path exactly matches "/". as / is a subtree pattern
+	// it is not good to route all not found paths to this rout
+
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+
+	// if the path is correct we will display this
+
 	w.Write([]byte("Hello from Snippet box"))
+}
+
+func snippetView(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet"))
 }
 
 func main() {
@@ -14,6 +33,9 @@ func main() {
 	mux := http.NewServeMux()
 	// register home as a function handler for /
 	mux.HandleFunc("/", home)
+
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	log.Print("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
