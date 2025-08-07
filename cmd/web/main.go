a894daa6b4d35c4e5257ad914702284ab9183e62
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-08-07 12:48:34 -0300

    add: leveled logging and parse port from cmdline argument

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/solomonsitotaw23/snippetbox/cmd/middleware"
 )
 
 func main() {
+	//accepting port / address where the server will run from a command line arg -addr=PORT
+	addr := flag.String("addr", ":4000", "Http network address")
+	flag.Parse()
+
+	// leveled logging
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
@@ -17,7 +27,7 @@ func main() {
 	mux.HandleFunc("/snippet/view", snippetView)
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
-	log.Print("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
-	log.Fatal(err)
+	infoLog.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
+	errorLog.Fatal(err)
 }
