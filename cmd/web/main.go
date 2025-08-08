8a98d9392f8e480e0278c67bcd1b7185466a0a70
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-08-08 07:33:04 -0300

    add: make the logger global using a dependency injection for all handlers

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -9,6 +9,11 @@ import (
 	"github.com/solomonsitotaw23/snippetbox/cmd/middleware"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog  *log.Logger
+}
+
 func main() {
 	//accepting port / address where the server will run from a command line arg -addr=PORT
 	addr := flag.String("addr", ":4000", "Http network address")
@@ -18,16 +23,29 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	app := &application{
+		errorLog: errorLog,
+		infoLog:  infoLog,
+	}
+
 	mux := http.NewServeMux()
 
 	fileServer := http.FileServer(http.Dir("./ui/static/"))
 	mux.Handle("/static/", http.StripPrefix("/static", middleware.Neuter(fileServer)))
 
-	mux.HandleFunc("/", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+
+	//since the http.Listen and serve uses the default logger we should change it to our custom logger
+	srv := &http.Server{
+		Addr:     *addr,
+		ErrorLog: errorLog,
+		Handler:  mux,
+	}
 
 	infoLog.Printf("Starting server on %s", *addr)
-	err := http.ListenAndServe(*addr, mux)
+
+	err := srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
