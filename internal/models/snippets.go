a84d5f55c4be43999fa45d3997f579bf8d2b730f
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-08-09 16:03:50 -0300

    Add: snippet models

---
+++ internal/models/snippets.go
@@ -0,0 +1,33 @@
+package models
+
+import (
+	"database/sql"
+	"time"
+)
+
+type Snippet struct {
+	ID      int
+	Title   string
+	Content string
+	Created time.Time
+	Expires time.Time
+}
+
+type SnippetModel struct {
+	DB *sql.DB
+}
+
+// Insert a new snippet
+func (m *SnippetModel) Insert(title string, content string, expires int) (int, error) {
+	return 0, nil
+}
+
+// return a specific snippet
+func (m *SnippetModel) Get(id int) (*Snippet, error) {
+	return nil, nil
+}
+
+// This will return the 10 most recently created snippets.
+func (m *SnippetModel) Latest() ([]*Snippet, error) {
+	return nil, nil
+}
