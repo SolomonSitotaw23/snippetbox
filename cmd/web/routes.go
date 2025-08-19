99d34933ba44b063d8f607cf3fb1b902c86a982c
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-08-19 07:44:48 -0300

    ADD: user management links on navbar

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -26,6 +26,14 @@ func (app *application) routes() http.Handler {
 	router.Handler(http.MethodGet, "/snippet/create", dynamic.ThenFunc(app.snippetCreate))
 	router.Handler(http.MethodPost, "/snippet/create", dynamic.ThenFunc(app.snippetCreatePost))
 
+	// user management
+
+	router.Handler(http.MethodGet, "/user/signup", dynamic.ThenFunc(app.userSignup))
+	router.Handler(http.MethodPost, "/user/signup", dynamic.ThenFunc(app.userSignupPost))
+	router.Handler(http.MethodGet, "/user/login", dynamic.ThenFunc(app.userLogin))
+	router.Handler(http.MethodPost, "/user/login", dynamic.ThenFunc(app.userLoginPost))
+	router.Handler(http.MethodPost, "/user/logout", dynamic.ThenFunc(app.userLogoutPost))
+
 	// standard middleware which will be used in every request
 	standard := alice.New(app.recoverPanic, app.logRequest, secureHeaders)
 
