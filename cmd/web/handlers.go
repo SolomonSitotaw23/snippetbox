58ec93b1f93512fece56e399dfaa9b7df60336f5
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-08-20 10:29:18 -0300

    ADD: user signup implmentation

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -146,7 +146,21 @@ func (app *application) userSignupPost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	fmt.Fprintln(w, "Create new user..")
+	err = app.users.Insert(form.Name, form.Email, form.Password)
+	if err != nil {
+		if errors.Is(err, models.ErrDuplicateEmail) {
+			form.AddFieldError("email", "email address already in use")
+			data := app.newTemplateData(r)
+			data.Form = form
+			app.render(w, http.StatusUnprocessableEntity, "signup.tmpl.html", data)
+		} else {
+			app.serverError(w, err)
+		}
+		return
+	}
+	// every thing works so lets add a conformation message
+	app.sessionManager.Put(r.Context(), "flash", "Your signup was successful. please login")
+	http.Redirect(w, r, "/user/login", http.StatusSeeOther)
 }
 
 func (app *application) userLogin(w http.ResponseWriter, r *http.Request) {
