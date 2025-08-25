048c239b12218406b95e8380a0832f0bce0b49fe
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-08-25 12:53:04 -0300

    ADD: read if the user is authenticated from the context

--- cmd/web/helpers.go
+++ cmd/web/helpers.go
@@ -84,5 +84,9 @@ func (app *application) decodePostForm(r *http.Request, dst any) error {
 // Authorization
 
 func (app *application) isAuthenticated(r *http.Request) bool {
-	return app.sessionManager.Exists(r.Context(), "authenticatedUserId")
+	isAuthenticated, ok := r.Context().Value(isAuthenticatedContextKey).(bool)
+	if !ok {
+		return false
+	}
+	return isAuthenticated
 }
