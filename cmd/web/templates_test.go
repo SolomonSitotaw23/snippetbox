a745aba075d7dcf71f8ce1498d33f180e564113a
solomonsitoatw23 <solomonsitotaw9@gmail.com>
2025-08-29 14:44:52 -0300

    REFACTOR: refactor the test a helper func

--- cmd/web/templates_test.go
+++ cmd/web/templates_test.go
@@ -3,6 +3,8 @@ package main
 import (
 	"testing"
 	"time"
+
+	"github.com/solomonsitotaw23/snippetbox/internal/assert"
 )
 
 func TestHumanDate(t *testing.T) {
@@ -32,10 +34,7 @@ func TestHumanDate(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			hd := humanDate(tt.tm)
-
-			if hd != tt.want {
-				t.Errorf("got %q; want %q", hd, tt.want)
-			}
+			assert.Equal(t, hd, tt.want)
 		})
 	}
 
